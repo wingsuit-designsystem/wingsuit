a3a783c7f35d689807ee4398bf696d2e8fd0002f
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-04-26 00:34:11 +0200

    Cleanup storybook.

--- packages/storybook/src/index.ts
+++ packages/storybook/src/index.ts
@@ -1,33 +1,38 @@
-import {storage} from '@wingsuit-designsystem/pattern';
+import { storage } from '@wingsuit-designsystem/pattern';
 
-import {storiesOf} from '@storybook/html';
-import {withKnobs, text, boolean, number, select} from "@storybook/addon-knobs";
+import { storiesOf } from '@storybook/html';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
 
 export function tellStories(patternId, module, callback) {
   const pattern = storage.loadPattern(patternId);
   const story = storiesOf(pattern.label, module);
   story.addDecorator(withKnobs);
-  Object.keys(pattern.patternVariants).forEach((key) => {
-    const variant = pattern.patternVariants[key];
+  Object.keys(pattern.getPatternVariants()).forEach((variantKey) => {
+    const variant = pattern.getVariant(variantKey);
     const variables = variant.getVariables();
-    story.add(variant.label, () => {
+    story.add(variant.getLabel(), () => {
       const knobsVariables = [];
-      Object.keys(variant.settings).forEach((key) => {
-        if (variant.settings[key].enable) {
-          if (variant.settings[key].type === 'select') {
-            knobsVariables[key] = select(variant.settings[key].label, variant.settings[key].options, variant.settings[key].preview);
-          } else if (variant.settings[key].type === 'boolean') {
-            knobsVariables[key] = boolean(variant.settings[key].label, variant.settings[key].preview);
-          } else if (variant.settings[key].type === 'number') {
-            knobsVariables[key] = number(variant.settings[key].label, variant.settings[key].preview);
+      Object.keys(variant.getSettings()).forEach((key) => {
+        const setting = variant.getSetting(key);
+        if (setting.enable()) {
+          if (setting.getType() === 'select') {
+            knobsVariables[key] = select(
+              setting.getLabel(),
+              setting.getOptions(),
+              setting.getPreview()
+            );
+          } else if (setting.getType() === 'boolean') {
+            knobsVariables[key] = boolean(setting.getLabel(), setting.getPreview());
+          } else if (setting.getType() === 'number') {
+            knobsVariables[key] = number(setting.getLabel(), setting.getPreview());
           } else {
-            knobsVariables[key] = text(variant.settings[key].label, variant.settings[key].preview);
+            knobsVariables[key] = text(setting.getLabel(), setting.getPreview());
           }
         }
       });
       const mergedSettingValues: {} = Object.assign(variables, knobsVariables);
       return callback(mergedSettingValues, variant);
     });
-  })
+  });
   return story;
 }
