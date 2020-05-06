38235d0cde819d51dc6fe69daab78b4bda2d8ba0
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-05-07 01:38:03 +0200

    v0.2.8

--- packages/wingsuit/apps/assets/webpack.config.js
+++
@@ -1,6 +0,0 @@
-/**
- * Wingsuit webpack config.
- */
-const wingsuit = require('@wingsuit-designsystem/core');
-const pack = wingsuit.getAppPack(process.env.NODE_ENV, module, []);
-module.exports = pack;
--- packages/wingsuit/apps/assets/wingsuit.app.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  namespaces: require('../../source/default/namespaces'),
-  type: "assets"
-};
