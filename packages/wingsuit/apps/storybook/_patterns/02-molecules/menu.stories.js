4f0e3da2291b95a00abda5eba67f9f40e07313fa
Michael Zemui <michael.zemui@key-tec.de>
2020-05-14 12:33:33 +0200

    WS #2: menu primary.

---
+++ packages/wingsuit/apps/storybook/_patterns/02-molecules/menu.stories.js
@@ -0,0 +1,9 @@
+import { renderer } from '@wingsuit-designsystem/pattern';
+
+export default { title: '02-molecules/menu' };
+
+const template = require('./menu.twig');
+
+export const PrimaryMenu = () => {
+  return renderer.renderData('path', template);
+};
