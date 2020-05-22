bfa514c0acd7b34ec5c67e595e5ea3d677f74037
Michael Zemui <michael.zemui@key-tec.de>
2020-05-22 16:15:15 +0200

    WS #00: total updates,

--- examples/official-wingsuit/apps/storybook/patterns/05-pages/article/article.stories.jsx
+++ examples/official-wingsuit/apps/storybook/patterns/05-pages/article/article.stories.jsx
@@ -1,6 +1,13 @@
+<<<<<<< HEAD:examples/official-wingsuit/apps/storybook/patterns/05-pages/article/article.stories.jsx
 import { RenderTwig } from '@wingsuit-designsystem/storybook';
 import React from 'react';
 import 'templates/article';
+=======
+import {RenderTwig} from "@wingsuit-designsystem/storybook";
+import React from "react";
+import "templates/article";
+
+>>>>>>> WS #00: total updates,:packages/wingsuit/apps/storybook/patterns/05-pages/article/article.stories.js
 
 export default {
   title: '05-pages/article',
