3ada686b3c9afc6a4b9efd85c8f6e89f91dbbe4c
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-09-26 12:35:36 +0200

    Initial commit.

--- packages/wingsuit/tailwind.config.js
+++ packages/wingsuit/tailwind.config.js
@@ -9,37 +9,23 @@ module.exports = {
     },
   },
   theme: {
+    extend: {
+      fontSize: {
+        // Final mobile:
+        ...rem(13),
+        ...rem(28),
+        ...rem(36),
+      },
+      maxWidth: {
+        none: 'none',
+        ...rem(800),
+        ...rem(1200),
+        ...rem(1600),
+      },
+    },
     fontFamily: {
       serif: ['SourceSerif', '-apple-system', 'BlinkMacSystemFont'],
       sans: ['Roboto', 'sans-serif'],
-    },
-    fontSize: {
-      // Final mobile:
-      ...rem(13),
-      ...rem(28),
-      ...rem(36),
-    },
-    maxWidth: {
-      none: 'none',
-      ...rem(800),
-      ...rem(1200),
-      ...rem(1600),
-    },
-    colors: {
-      transparent: 'transparent',
-      black: '#000000',
-      white: '#FFFFFF',
-      green: '#deede8',
-      blue: '#4299E1',
-      primary: '#fcaca9',
-      success: '#68D391',
-      yellow: '#F6E05E',
-      gray: {
-        xlight: '#EDF2F7',
-        light: '#CBD5E0',
-        medium: '#718096',
-        dark: '#2D3748',
-      },
     },
   },
   variants: {
