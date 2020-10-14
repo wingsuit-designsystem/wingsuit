a846e77e4db8b15a8148126dc60d974419861728
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-10-14 19:06:44 +0200

    Add layout builder settings.

---
+++ packages/wingsuit/apps/drupal/js/header-offset.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Adds the toolbar offset the fixed header component.
+ */
+(function($) {
+  Drupal.behaviors.header_offset = {
+    attach() {
+      if (typeof $ !== 'undefined') {
+        $(document).on('drupalViewportOffsetChange', function (event, offset) {
+          $('.header-offset').css('top', offset.top);
+        });
+      }
+    },
+  };
+})(jQuery);
