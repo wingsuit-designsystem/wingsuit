b2be4c29c10af2b9025b735005ac1c23d30b2d49
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-11-25 23:24:27 +0100

    (Feature) Update spacing doc component.

--- packages/storybook/src/docs/Spacing.tsx
+++ packages/storybook/src/docs/Spacing.tsx
@@ -1,51 +1,82 @@
-import React, {FunctionComponent} from 'react';
-import {ScrollArea} from '@storybook/components';
+import React, { FunctionComponent } from 'react';
+import { ScrollArea } from '@storybook/components';
 
 interface SpacingProps {
   spacing: {};
-  color?
-  direction?
-  classNamePrefix?
+  color?;
+  direction?;
+  classNamePrefix?;
 }
 
 export const Spacing: FunctionComponent<SpacingProps> = (props: SpacingProps) => {
-  const {direction, spacing, classNamePrefix, color} = props;
+  const { direction, spacing, classNamePrefix, color } = props;
   return direction === 'horizontal' ? (
     <ScrollArea>
       {Object.keys(spacing).map((key, i) => {
         return (
-          <div style={{display: 'flex', paddingBottom: '6px'}}>
-            <div style={{width: '60px', fontSize: '10px', flexShrink: 0}}>
-              {classNamePrefix != null ? (<div style={{paddingTop: '4px'}}>{classNamePrefix + key}</div>) : (
-                <div style={{paddingTop: '4px'}}>{spacing[key]}</div>)}
+          <div style={{ display: 'flex', paddingBottom: '6px' }}>
+            <div style={{ width: '60px', fontSize: '10px', flexShrink: 0 }}>
+              {classNamePrefix != null ? (
+                <div style={{ paddingTop: '4px' }}>{classNamePrefix + key}</div>
+              ) : (
+                <div style={{ paddingTop: '4px' }}>{spacing[key]}</div>
+              )}
             </div>
-            <div style={{width: '100%'}}>
-              <div className={classNamePrefix + key}
-                   style={{height: '1.4rem', backgroundColor: color, width: spacing[key], borderRadius: '0.375rem'}}>
+            <div style={{ width: '100%' }}>
+              <div
+                className={classNamePrefix + key}
+                style={{
+                  height: '1.4rem',
+                  backgroundColor: color,
+                  width: spacing[key],
+                  borderRadius: '0.375rem',
+                }}
+              >
                 {classNamePrefix != null ? (
-                  <div style={{fontSize: '10px', paddingTop: '4px', marginLeft: '8px'}}>{spacing[key]}</div>) : null}
+                  <div style={{ fontSize: '10px', paddingTop: '4px', marginLeft: '8px' }}>
+                    {spacing[key]}
+                  </div>
+                ) : null}
               </div>
             </div>
           </div>
-        )
+        );
       })}
     </ScrollArea>
   ) : (
-
     <ScrollArea>
-      <div style={{display: 'flex'}}>
+      <div style={{ display: 'flex' }}>
         {Object.keys(spacing).map((key, i) => {
           return (
-            <div style={{paddingBottom: '6px'}}>
-              <div style={{width: '38px', fontSize: '11px', whiteSpace: 'nowrap', 'overflow': 'hidden', textOverflow: 'ellipsis'}}>
-                {classNamePrefix != null ? (<div>{classNamePrefix + key}</div>) : (<div>{spacing[key]}</div>)}
+            <div style={{ paddingBottom: '6px' }}>
+              <div
+                style={{
+                  width: '38px',
+                  fontSize: '11px',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                {classNamePrefix != null ? (
+                  <div>{classNamePrefix + key}</div>
+                ) : (
+                  <div>{spacing[key]}</div>
+                )}
               </div>
-              <div style={{height: '100%'}}>
-                <div className={classNamePrefix + key}
-                     style={{backgroundColor: color, width: '1.4rem', height: spacing[key], borderRadius: '0.375rem'}}/>
+              <div style={{ height: '100%' }}>
+                <div
+                  className={classNamePrefix + key}
+                  style={{
+                    backgroundColor: color,
+                    width: '1.4rem',
+                    height: spacing[key],
+                    borderRadius: '0.375rem',
+                  }}
+                />
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </ScrollArea>
@@ -55,8 +86,7 @@ export const Spacing: FunctionComponent<SpacingProps> = (props: SpacingProps) =>
 Spacing.defaultProps = {
   classNamePrefix: null,
   color: '#81E6D9',
-  direction: 'horizontal'
+  direction: 'horizontal',
 };
-
 
 export default Spacing;
