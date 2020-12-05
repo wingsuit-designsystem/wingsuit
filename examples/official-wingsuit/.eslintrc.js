9fddda9b4fc49e6b1b7198ccd510819d187c1d22
Christian Wiedemann <christian.wiedemann@key-tec.de>
2020-12-05 22:55:46 +0100

    (fix) Update examples.

--- examples/official-wingsuit/.eslintrc.js
+++ examples/official-wingsuit/.eslintrc.js
@@ -4,10 +4,19 @@
  * Rule reference: http://eslint.org/docs/rules
  * Individual rule reference: http://eslint.org/docs/rules/NAME-OF-RULE
  */
-const path = require('path');
-const namespaces = require('./source/default/namespaces');
+const wingsuitCore = require('@wingsuit-designsystem/core');
+const wingsuitConfig = require('./wingsuit.config');
+
+const appConfig = wingsuitCore.resolveConfig(
+  'storybook',
+  'development',
+  {},
+  wingsuitConfig,
+  __dirname
+);
+const { namespaces } = appConfig;
 const aliasMap = [];
-Object.keys(namespaces).forEach((key)=>{
+Object.keys(namespaces).forEach((key) => {
   aliasMap.push([key, namespaces[key]]);
 });
 
