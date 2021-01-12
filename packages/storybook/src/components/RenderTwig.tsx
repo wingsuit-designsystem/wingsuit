febd1deeffef91f9615fbc7f6f9e2d7f20f45316
Christian Wiedemann <christian.wiedemann@key-tec.de>
2021-01-12 16:22:11 +0100

    (feature) Add new javascript behavoir logic to RenderTwig.

--- packages/storybook/src/components/RenderTwig.tsx
+++ packages/storybook/src/components/RenderTwig.tsx
@@ -1,21 +1,28 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { renderer } from '@wingsuit-designsystem/pattern';
+import {attachBehaviors} from "../behaviors";
 
 type Props = { data };
 
 const RenderTwig: FunctionComponent<Props> = ({ data, ...variables }) => {
   const [rendered, setRendered] = useState('');
   useEffect(() => {
+    let mounted = true;
     renderer
       .renderData(Math.random().toString(), data.default, variables)
       .then((output: string) => {
-        setRendered(output);
+        if (mounted) {
+          setRendered(output);
+        }
       })
       .catch((error) => {
         setRendered(error.message);
       });
   }, [data, JSON.stringify(variables)]);
-
+  useEffect(() => {
+    if (!rendered) return;
+    attachBehaviors(global.window.document, {});
+  }, [rendered]);
   // eslint-disable-next-line react/no-danger
   return <div dangerouslySetInnerHTML={{ __html: rendered }} />;
 };
