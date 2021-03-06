37e5bb4f051778df95202c28ff3ec45e98ef4839
Christian Wiedemann <christian.wiedemann@key-tec.de>
2021-03-06 23:54:41 +0100

    Switch from knobs to controls.

--- starter-kits/bootstrap/apps/storybook/main.js
+++ starter-kits/bootstrap/apps/storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-viewport',
-    '@storybook/addon-knobs',
+    '@storybook/addon-controls',
     '@storybook/addon-links',
     '@storybook/addon-backgrounds',
   ],
