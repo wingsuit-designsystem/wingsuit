121eb0073778ee680120596c4c653cab4fd42b1a
Christian Wiedemann <christian.wiedemann@key-tec.de>
2021-03-31 10:47:42 +0200

    (fix) Remove hightlight.js from babel config for storybook 6.2.

--- packages/core/src/server/presets/babel.ts
+++ packages/core/src/server/presets/babel.ts
@@ -20,7 +20,7 @@ export function webpack(appConfig: AppConfig) {
         {
           test: /\.js$/,
           // @babel runtime and core must NOT be transformed by babel
-          exclude: /@babel(?:\/|\\{1,2})runtime|core-js/,
+          exclude: /@babel(?:\/|\\{1,2})runtime|core-js|highlight\.js/,
           use: {
             loader: 'babel-loader',
             options: {
