95da51461b322da9a995c033b420ad8031e23e29
Christian Wiedemann <christian.wiedemann@key-tec.de>
2021-09-17 18:13:07 +0200

    Update docs deploy script.

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,5 +1,6 @@
 yarn build:pages
 cp -R dist/app-pages/* ../../wingsuit-designsystem.github.io
 cd ../../wingsuit-designsystem.github.io
-git commit -a -m "deploy"
+git add .
+git commit -a -m "Deploy: $(date +'%Y-%m-%d')"
 git push
