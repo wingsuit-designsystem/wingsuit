941150344d81a10d24f4e77571a80499064e7115
Christian Wiedemann <christian.wiedemann@key-tec.de>
2023-03-26 11:17:21 +0200

    fix: Fix e2e tests

--- cypress/e2e/wsc.ts
+++ cypress/e2e/wsc.ts
@@ -1,18 +1,21 @@
 import { clickAddon, visit } from '../helper';
 
-describe('Wingsuit Component', () => {
+describe('Wingsuit Default Component', () => {
   beforeEach(() => {
-    visit('official-wingsuit/?path=/story/pages-ws-apps-component--apps-ws-component-default-pattern');
+    visit('official-wingsuit/?path=/story/components-ws-source-component--source-ws-component-default-pattern');
   });
 
   it('[text] it should change field value.', () => {
     clickAddon('Controls');
+    // Test Textfields
+    cy.get('#set-setting_textfield').click().get('#control-setting_textfield').clear().type('John Doe');
+    cy.get('#control-setting_select').select('setting_select_options1');
 
-    cy.get('#control-setting_textfield').clear().type('John Doe');
     cy.wait(1000);
     cy.getStoryElement()
       .find('.wsc-component')
       .eq(0)
-      .should('contain.text', 'setting_textfield: John Doe');
+      .should('contain.text', 'setting_textfield: John Doe')
+      .should('contain.text', 'setting_select: setting_select_options1')
   });
 });
