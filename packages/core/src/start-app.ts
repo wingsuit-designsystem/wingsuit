05697c12ef73ca4f76e00484569ccbdd61e7e563
Christian Wiedemann <christian.wiedemann@key-tec.de>
2023-04-23 23:06:01 +0200

    fix: Resolve build storybooks

--- packages/core/src/start-app.ts
+++ packages/core/src/start-app.ts
@@ -1,7 +1,11 @@
 /**
  * Wingsuit webpack config.
  */
-import { resolveConfig, getAppPack } from './index'; 
-const appConfig = resolveConfig(process.env.WINGSUIT_APP ?? 'storybook', process.env.NODE_ENV ?? 'develop');
+import { resolveConfig, getAppPack } from './index';
+
+const appConfig = resolveConfig(
+  process.env.WINGSUIT_APP ?? 'storybook',
+  process.env.NODE_ENV ?? 'develop'
+);
 const finalConfig = getAppPack(appConfig);
 module.exports = finalConfig;
