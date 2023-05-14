eb8fe8e0a903095922cdb0d51619048eadea1f03
Christian Wiedemann <christian.wiedemann@key-tec.de>
2023-05-15 00:03:36 +0200

    fix: Add unique presets test

--- docs/apps/pages/webpack.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wingsuit webpack config.
- */
-const wingsuitCore = require('@wingsuit-designsystem/core');
-
-const appConfig = wingsuitCore.resolveConfig('pages', process.env.NODE_ENV);
-const finalConfig = wingsuitCore.getAppPack(appConfig);
-module.exports = finalConfig;
