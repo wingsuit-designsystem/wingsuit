048844e62572dd62a5d522b621e841a9a57ca534
Christian Wiedemann <christian.wiedemann@key-tec.de>
2024-06-03 00:25:42 +0200

    fix: Update package json for version 2

--- starter-kits/tailwind/apps/storybook/manager.js
+++ starter-kits/tailwind/apps/storybook/manager.js
@@ -1,6 +1,6 @@
 // .storybook/manager.js
 
-import { addons } from '@storybook/addons';
+import { addons } from '@storybook/manager-api';
 import { theme } from '@wingsuit-designsystem/storybook/theming';
 
 addons.setConfig({
